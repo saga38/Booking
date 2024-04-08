cb86585f06f3a65dc7034f18902f6e7d5539fe15
unknown <luislopezoliva@infoutilitygt.com>
2024-04-08 10:00:02 -0600

    Primer push de proyecto de spring boot

---
+++ src/main/java/controller/HealthController.java
@@ -0,0 +1,10 @@
+package controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController public class HealthController {
+ @RequestMapping("/health")
+     public String checkAPI(){
+        return "<h1>The API is working ok!</h1>";
+     }
+
+}
